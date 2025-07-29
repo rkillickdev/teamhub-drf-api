9cf8a586f0c3decc930bef42e99281f13bbf12cf
Rob Killick <robkillick@Robs-MBP-2.home>
2025-07-29 13:55:39 +0100

    create teams model and make migrations

---
+++ src/teams/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+
+
+class Team(models.Model):
+    """Team object"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    manager = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
