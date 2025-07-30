fc89b1318c500fdc2cc585b14c4e125279ea38fb
Rob Killick <robkillick@Robs-MBP-2.home>
2025-07-30 10:42:24 +0100

    create serializer for teams APIs

---
+++ src/teams/serializers.py
@@ -0,0 +1,22 @@
+"""
+Serializers for teams APIs
+"""
+
+from rest_framework import serializers
+
+from models import Team
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    """Serializer for teams."""
+
+    class Meta:
+        model = Team
+        fields = [
+            "id",
+            "name",
+            "manager",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["id"]
