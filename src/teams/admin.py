97af08beda41f468dd812ef42ae1115f6f9b564d
Rob Killick <robkillick@Robs-MBP-2.home>
2025-07-30 13:39:11 +0100

    add Player model and make migrations

--- src/teams/admin.py
+++ src/teams/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Team
+from .models import Player
 
 admin.site.register(Team)
+admin.site.register(Player)
--- src/teams/models.py
+++ src/teams/models.py
@@ -13,6 +13,24 @@ class Team(models.Model):
     manager = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    players = models.ManyToManyField("Player")
 
     def __str__(self):
         return self.name
+
+
+class Player(models.Model):
+    """Player object"""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
